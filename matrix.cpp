f4013f68c87fbe8e0bd7585dc36f2d90dc09afab
Владимир Соловьев <solvov2006@gmail.com>
2024-09-26 00:19:00 +0500

    initial commit

---
+++ matrix.cpp
@@ -0,0 +1,38 @@
+#include "matrix.h"
+#include <iostream>
+
+
+double product(Matrix A, Matrix B, Matrix C);
+
+int main() {
+    Matrix mat1(2, 2);
+    mat1(0, 0) = 1; mat1(0, 1) = 2;
+    mat1(1, 0) = 3; mat1(1, 1) = 4;
+
+    Matrix mat2(2, 2);
+    mat2(0, 0) = 5; mat2(0, 1) = 6;
+    mat2(1, 0) = 7; mat2(1, 1) = 8;
+
+    Matrix mat5(2, 3);
+    mat5(0, 0) = 5; mat5(0, 1) = 6; mat5(0,2) = 10;
+    mat5(1, 0) = 7; mat5(1, 1) = 8; mat5(1,2) = 100;
+
+    Matrix m3 = mat1 + mat2;
+    std::cout << "This is Matrix m3:" << std::endl;
+    m3.printMatrix();
+
+    Matrix m4 = mat1 * mat2;
+    std::cout << "This is Matrix m4:" << std::endl;
+    m4.printMatrix();
+
+    Matrix m6 = mat5.transposition();
+
+    std::cout << "This is Matrix m6:" << std::endl;
+    m6.printMatrix();
+    double c = product(m3, m4, mat1);
+
+    std::cout << "This is matrix C" << c << std::endl;
+    return 0;
+}
+
+
---
+++ matrix.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <cmath>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+
+
+class Matrix {
+
+public:
+    unsigned lin;
+    unsigned col;
+    std::vector <std::vector<double> > Mat;
+    Matrix(unsigned l, unsigned c) : lin(l), col(c), Mat(l, std::vector<double>(c)) {}
+
+    // Matrix(unsigned l, unsigned c, std::vector <std::vector<double> > M(l, std::vector<double>(c))) :
+    //     lin(l), col(c), 
+        
+
+
+    double& operator() (int i, int j) {return Mat[i][j];}
+
+    void printMatrix () const {
+        for(unsigned i = 0; i < lin;i++) {
+            for (unsigned j = 0; j < col; j++) {
+                std::cout << Mat[i][j] << "  ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    Matrix operator+(const Matrix& M) const {
+        assert(lin == M.lin && col == M.col);
+        Matrix result(lin, col);
+        for(unsigned i =0; i < lin; i++) {
+            for(unsigned j = 0; j < col; j++) {
+                result(i,j) = Mat[i][j] + M.Mat[i][j];
+            }
+        }
+        return result;
+    }
+    Matrix& operator=(const Matrix& other) {
+        for(unsigned i =0; i < lin; i++) {
+            for(unsigned j = 0; j < col; j++) {
+                Mat[i][j] = other.Mat[i][j];
+            }
+        }
+        return *this;
+    }
+    Matrix operator*(const Matrix& M) const {
+        assert(col == M.lin);
+        Matrix result (lin, M.col);
+        for(unsigned i =0; i < lin; i++) {
+            for(unsigned k = 0; k < M.col; k++) {
+                result(i,k) = 0.;
+                for(unsigned j = 0; j < col; j++) {
+                    result(i,k) += Mat[i][j]*Mat[j][k];
+                }
+            }
+        }
+        return result;
+    }
+
+    Matrix transposition() const {
+        Matrix M(col, lin);
+        for(unsigned i = 0; i < col; i++) {
+            for(unsigned j = 0; j < lin; j++) {
+                M.Mat[i][j] = Mat[j][i];
+            }
+        }
+        return M;
+    }
+
+};
+
+double product(Matrix A, Matrix B, Matrix C);
